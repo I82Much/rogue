50fd8b31d634774c0bde5eda12b9ec6f25349551
Nick Dunn <nicholas.c.dunn@gmail.com>
2014-03-09 23:08:50 -0700

    I have a falling letter example working with go routine pulling from xterm and pushing to a mutex protected slice

--- falling_words/main.go
+++ falling_words/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"sync"
 	"time"
 
 	termbox "github.com/nsf/termbox-go"
@@ -9,36 +11,44 @@ import (
 var (
 	runFlag = false
 	maxRows = 21
-	words  = []*Word{
+	words   = []*Word{
 		&Word{
-			word: "Hello",
-			row: 0,
+			word:     "Hello",
+			row:      0,
 			onScreen: time.Now(),
 			duration: time.Duration(3) * time.Second,
 		},
 		&Word{
-			word: "World",
-			row: 2,
+			word:     "World",
+			row:      2,
 			onScreen: time.Now(),
 			duration: time.Duration(4) * time.Second,
 		},
 		&Word{
-			word: "Supercalifragilistic",
-			row: 4,
+			word:     "Supercalifragilistic",
+			row:      4,
 			onScreen: time.Now(),
-			duration: time.Duration(2) * time.Second,
+			duration: time.Duration(10) * time.Second,
 		},
 	}
+
+	currentTyping  *Word
+	hits, attempts int
+
+	completedWords int
+
+	unprocessedRunes = []rune{}
+	// Protects unprocessedRunes slice
+	runesMutex sync.Mutex
 )
 
 type Word struct {
-	word string
-	row int
-	onScreen time.Time 
+	word     string
+	spelled  []rune
+	row      int
+	onScreen time.Time
 	duration time.Duration
 }
-
-
 
 /**
  * @param value The incoming value to be converted
@@ -69,10 +79,76 @@ func startup() {
 		panic(err)
 	}
 	termbox.HideCursor()
+	// Run the input polling loop in another goroutine
+	go input()
+}
+
+func input() {
+	for runFlag {
+		event := termbox.PollEvent()
+		if event.Key == termbox.KeyCtrlC {
+			stop()
+		}
+		// They typed a letter
+		if event.Ch != 0 {
+			runesMutex.Lock()
+			// TODO(ndunn): probably could/should use channels for this
+			unprocessedRunes = append(unprocessedRunes, event.Ch)
+			runesMutex.Unlock()
+		}
+	}
+}
+
+func removeWord(w *Word) {
+	index := -1
+	for i, word := range words {
+		if word == w {
+			words = append(words[0:i], words[i+1:]...)
+			return
+		}
+	}
+	if index == -1 {
+		panic(fmt.Sprintf("couldn't find word %v", w))
+	}
 }
 
 func update() {
 	now := time.Now()
+
+	// Pull out the unprocessed runes, apply them to the words
+	runesMutex.Lock()
+	for _, r := range unprocessedRunes {
+		attempts++
+		// Does this rune represent the first untyped letter of any of the candidates? If so it's a hit. If not it's a miss
+		if currentTyping != nil {
+			runes := []rune(currentTyping.word)
+			if r == runes[len(currentTyping.spelled)] {
+				hits++
+				currentTyping.spelled = append(currentTyping.spelled, r)
+
+				// Done the word
+				if len(currentTyping.spelled) == len(currentTyping.word) {
+					removeWord(currentTyping)
+					completedWords++
+					currentTyping = nil
+				}
+			}
+		} else {
+			for _, word := range words {
+				runes := []rune(word.word)
+				if r == runes[len(word.spelled)] {
+					hits++
+					word.spelled = append(word.spelled, r)
+					currentTyping = word
+					break
+				}
+			}
+		}
+	}
+	// Clear out unprocessed runes - they've been processed
+	unprocessedRunes = nil
+	runesMutex.Unlock()
+
 	anyOnscreen := false
 	for _, word := range words {
 		elapsed := now.Sub(word.onScreen)
@@ -91,10 +167,32 @@ func update() {
 func draw() {
 	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
 	for _, word := range words {
+		foreground := termbox.ColorDefault
+		if word == currentTyping {
+			foreground = foreground | termbox.AttrBold
+		}
 		for i, c := range word.word {
-			termbox.SetCell(i, word.row, c, termbox.ColorDefault, termbox.ColorDefault)
+			if i < len(word.spelled) {
+				termbox.SetCell(i, word.row, c, termbox.ColorRed|termbox.AttrBold, termbox.ColorDefault)
+			} else {
+				termbox.SetCell(i, word.row, c, foreground, termbox.ColorDefault)
+			}
 		}
 	}
+
+	runesMutex.Lock()
+	for i, r := range unprocessedRunes {
+		termbox.SetCell(i, 0, r, termbox.ColorRed, termbox.ColorDefault)
+	}
+	runesMutex.Unlock()
+
+	if attempts > 0 {
+		accuracyText := fmt.Sprintf("%d / %d (%.2f%%)", hits, attempts, 100.0*float32(hits)/float32(attempts))
+		for i, c := range accuracyText {
+			termbox.SetCell(i, 3, c, termbox.ColorDefault, termbox.ColorDefault)
+		}
+	}
+
 	termbox.Flush()
 }
 
