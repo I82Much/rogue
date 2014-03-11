3eaba134bd904465a219d6bee3b535f36e234857
Nick Dunn <nicholas.c.dunn@gmail.com>
2014-03-10 23:46:14 -0700

    Committing the controller model view

---
+++ combat/model.go
@@ -0,0 +1,145 @@
+package combat
+
+import (
+	"fmt"
+	"time"
+
+	termbox "github.com/nsf/termbox-go"
+	"github.com/I82Much/rogue/math"
+)
+
+type CombatModel struct {
+	Monsters []*Monster
+	Player   *Player
+
+	words []*AttackWord
+
+	attempts       int
+	hits           int
+	completedWords int
+	currentTyping  *AttackWord
+}
+
+func NewCombatModel(p *Player, m []*Monster) *CombatModel {
+	var allWords []*AttackWord
+	for _, m1 := range m {
+		allWords = append(allWords, m1.Words...)
+	}
+	return &CombatModel{
+		Monsters: m,
+		Player:   p,
+		words:    allWords,
+	}
+}
+
+type AttackWord struct {
+	word    string
+	spelled []rune
+	// TODO(ndunn): The row seems like part of view.. not sure though
+	row      int
+	maxRows  int
+	onScreen time.Time
+	duration time.Duration
+}
+
+func (w *AttackWord) Damage() int {
+	return len(w.word)
+}
+
+func NewWord(word string, dur time.Duration) *AttackWord {
+	return &AttackWord{
+		word:     word,
+		maxRows:  25,
+		onScreen: time.Now(),
+		duration: dur,
+	}
+}
+
+func (c *CombatModel) Words() []*AttackWord {
+	return c.words
+}
+
+func (c *CombatModel) CurrentlyTyping() *AttackWord {
+	return c.currentTyping
+}
+
+// Over determines if the fight is over. Meaning either all enemies are dead, or player is dead
+func (c *CombatModel) Over() bool {
+	if c.Player.IsDead() {
+		return true
+	}
+	// If any monster is left, fight's not over
+	for _, m := range c.Monsters {
+		if !m.IsDead() {
+			return false
+		}
+	}
+	return true
+}
+
+// KillWord removes the word from model, meaning that's it vanquished
+func (c *CombatModel) KillWord(w *AttackWord) {
+	// TODO(ndunn): score? update exp?
+	for i, word := range c.words {
+		if word == w {
+			c.words = append(c.words[0:i], c.words[i+1:]...)
+			return
+		}
+	}
+	// The terminal gets really screwed up if we don't shut down termbox first
+	termbox.Close()
+	panic(fmt.Sprintf("couldn't find word %v", w))
+}
+
+func (c *CombatModel) DamagePlayer(w *AttackWord) {
+	c.Player.Damage(w.Damage())
+}
+
+func (c *CombatModel) Update(typed []rune) {
+	now := time.Now()
+	for _, r := range typed {
+		c.attempts++
+		// Does this rune represent the first untyped letter of any of the candidates? If so it's a hit. If not it's a miss
+		if c.currentTyping != nil {
+			runes := []rune(c.currentTyping.word)
+			if r == runes[len(c.currentTyping.spelled)] {
+				c.hits++
+				c.currentTyping.spelled = append(c.currentTyping.spelled, r)
+
+				// Done the word
+				if len(c.currentTyping.spelled) == len(c.currentTyping.word) {
+					c.KillWord(c.currentTyping)
+					c.completedWords++
+					c.currentTyping = nil
+				}
+			}
+		} else {
+			// See if the rune matches first letter of one of our candidate words
+			for _, word := range c.Words() {
+				runes := []rune(word.word)
+				if r == runes[len(word.spelled)] {
+					c.hits++
+					word.spelled = append(word.spelled, r)
+					c.currentTyping = word
+					break
+				}
+			}
+		}
+	}
+
+	var toRemove []*AttackWord
+	for _, word := range c.words {
+		elapsed := now.Sub(word.onScreen)
+		row := math.IntMap(int(elapsed.Nanoseconds()), 0.0, int(word.duration.Nanoseconds()), 0, word.maxRows-1)
+		word.row = row
+
+		// Inflict damage on the player
+		if row > word.maxRows {
+			c.DamagePlayer(word)
+			toRemove = append(toRemove, word)
+		}
+	}
+	for _, word := range toRemove {
+		c.KillWord(word)
+	}
+}
---
+++ combat/view.go
@@ -0,0 +1,115 @@
+package combat
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/I82Much/rogue/math"
+	termbox "github.com/nsf/termbox-go"
+)
+
+type CombatView struct {
+	Model *CombatModel
+	rows  int
+}
+
+// Render assumes that termbox has already been initialized.
+func (v *CombatView) Render() {
+	// Draw all of the falling words
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+
+	// Render the monsters
+	for i, monster := range v.Model.Monsters {
+		monsterFigure := `
+		 \ O /
+		   |
+		  / \`
+		healthBarWidth := 25
+
+		offset := i * (10 + healthBarWidth)
+
+		for row, figure := range strings.Split(monsterFigure, "\n") {
+			for j, char := range figure {
+				termbox.SetCell(offset+j, row, char, termbox.ColorDefault, termbox.ColorDefault)
+			}
+		}
+		// Draw the health bar
+		healthWidth := math.IntMap(monster.Life, 0, monster.MaxLife, 0, healthBarWidth)
+		if healthWidth < 0 {
+			healthWidth = 0
+		}
+		for h := 0; h < healthWidth; h++ {
+			termbox.SetCell(offset+5+h, 0, '█', termbox.ColorRed, termbox.ColorDefault)
+		}
+	}
+
+	// Render the player
+
+	// TODO(ndunn): all of the monster graphics etc should be moved into template files
+	playerFigure := `
+                                               +
+                +--------------+               |
+                |              |               |
+                |              |               |
++---------+     |              |               |
+|         |     |              |               |
+|         |     |              |            +----->
+|         |     |              |               |
+|         | +----------------------+           |
+|    ^    | |                      |           |
+|    |    | |                      |           |
+|    |    | |                      +-----------+
++----|----+ |                      +
+     +------+                      |
+            |                      |
+            |                      |
+            +----------------------+
+	
+	`
+	for row, figure := range strings.Split(playerFigure, "\n") {
+		for j, char := range figure {
+			finalRow := 10 + row
+			termbox.SetCell(j, finalRow, char, termbox.ColorDefault, termbox.ColorDefault)
+		}
+	}
+	// Draw player's health bar
+	healthWidth := 40
+	player := v.Model.Player
+	life := math.IntMap(player.Life, 0, player.MaxLife, 0, healthWidth)
+	if life < 0 {
+		life = 0
+	}
+	for j := 0; j < healthWidth; j++ {
+		row := 15
+		if j < life {
+			termbox.SetCell(j, row, '█', termbox.ColorRed, termbox.ColorDefault)
+		} else {
+			termbox.SetCell(j, row, '░', termbox.ColorDefault, termbox.ColorDefault)
+		}
+	}
+
+	for _, word := range v.Model.Words() {
+		foreground := termbox.ColorDefault
+		if word == v.Model.CurrentlyTyping() {
+			foreground = foreground | termbox.AttrBold
+		}
+		for i, c := range word.word {
+			if i < len(word.spelled) {
+				termbox.SetCell(i, word.row, c, termbox.ColorRed|termbox.AttrBold, termbox.ColorDefault)
+			} else {
+				termbox.SetCell(i, word.row, c, foreground, termbox.ColorDefault)
+			}
+		}
+	}
+
+	// Draw the % accuracy
+	if v.Model.attempts > 0 {
+		hits := v.Model.hits
+		attempts := v.Model.attempts
+		accuracyText := fmt.Sprintf("%d / %d (%.2f%%)", hits, attempts, 100.0*float32(hits)/float32(attempts))
+		for i, c := range accuracyText {
+			termbox.SetCell(i, 3, c, termbox.ColorDefault, termbox.ColorDefault)
+		}
+	}
+	termbox.Flush()
+}
