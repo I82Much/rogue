35ab3bfa9cc3377cfdf99eeede372c21de2519d7
Nick Dunn <nicholas.c.dunn@gmail.com>
2014-03-13 21:23:12 -0700

    Adding support for WPM

--- combat/attack_words.go
+++ combat/attack_words.go
@@ -1,6 +1,7 @@
 package combat
 
 import (
+	"fmt"
 	"time"
 )
 
@@ -35,6 +36,35 @@ func NewWord(word string, dur time.Duration, initialDelay time.Duration) AttackW
 		duration:     dur,
 		Col:          Left,
 	}
+}
+
+// phrases - the phrases to convert into the attacks
+// words per minute - target number of words per minute. Assumes 5 characters per word, 
+// including spaces and punctuation (see wikipedia en.m.wikipedia.org/wiki/Words_per_minute)
+// delay - how much extra time should be inserted between launches of attacks? 
+// TODO(ndunn): 
+func AttackWords(phrases []string, wordsPerMinute int, delay time.Duration) []*AttackWord {
+	if wordsPerMinute <= 0 {
+		panic(fmt.Sprintf("need wpm > 0; got %d", wordsPerMinute))
+	}
+
+	var totalDelay = delay
+	var attacks []*AttackWord
+	// characters per minute
+	cpm := wordsPerMinute * 5
+	for _, phrase := range phrases {
+		chars := len(phrase)
+		minutes := float32(chars) / float32(cpm)
+		seconds := minutes / 60.0
+		timeOnScreen := time.Duration(int(1000 * seconds)) * time.Millisecond
+		
+		attack := NewWord(phrase, timeOnScreen, totalDelay)
+		attacks = append(attacks, &attack)
+		
+		// FIXME should be able to have some overlap on the screen
+		totalDelay = time.Duration(totalDelay.Nanoseconds() + delay.Nanoseconds() + timeOnScreen.Nanoseconds())
+	}
+	return attacks
 }
 
 func (w *AttackWord) Damage() int {
